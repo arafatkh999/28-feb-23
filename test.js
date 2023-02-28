627ad196422402cf2f5a0918fb964800c48f1246
arafatkh999 <arafatkh999@gmail.com>
2023-02-28 12:14:12 +0600

    practice problem

---
+++ test.js
@@ -0,0 +1,71 @@
+// const a =[];
+// const b =true;
+// if(a==b){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+// const result = a==b?true:false;
+// console.log(result);
+
+// const output = true==='true'?true:false;
+// console.log(output);
+
+// const arr=n=>{
+// // return isNaN(n)? true:false
+// // if(isNaN(n)){
+// //     return `${n} is not a number`
+// // }
+// // else{
+// //     return `${n} is a number`
+// // }
+
+// // if(typeof n ==='number'){
+// //     return `${n} is a number`
+// // }
+// // else{
+// //     return `${n} is not a number`
+// // }
+// };
+// const output=arr(5);
+// console.log(output);
+
+
+// let storeFalsyValue = true;
+// if(!storeFalsyValue) {
+//  console.log(`Nothing to show!`);
+// } 
+// else {
+//  console.log(`You are amazing!`);
+// }
+
+// const arr=(num1,num2,output)=>{
+// let result;
+// if(output==='add'){
+//     result=num1+num2;
+// }
+// else if(output==='subtract'){
+//     result = num1-num2;
+// }
+// else if(output==='multiply'){
+//     result=num1*num2;
+// }
+// else if(output=== 'divide'){
+//     result=num1/num2;
+// }
+// else if(output==='modulus'){
+//     result=num1%num2;
+// }
+// else{
+//     return('Invalid operation');
+// }
+
+// return `The result is ${result}`;
+
+
+// }
+
+// const output = arr(2,4)
+// console.log(output);
